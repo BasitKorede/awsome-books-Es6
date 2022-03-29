9bd034047fcc014ea509ea82fd3af08bfa41e8a5
Basit Arigbabuwo <arigbabuwobasitkorede6@gmail.com>
2022-03-29 01:22:14 +0100

    Add modulefunctionalities

---
+++ js/modules/local-storage.js
@@ -0,0 +1,36 @@
+export default class BookStore {
+    constructor(initialData = []) {
+      this.saveToLocalStorage = (data) => {
+        const booksString = JSON.stringify(data);
+        localStorage.setItem('bookStoreData', booksString);
+        return true;
+      };
+  
+      const rawBooksData = localStorage.getItem('bookStoreData');
+      if (rawBooksData) {
+        this.books = JSON.parse(rawBooksData);
+      } else {
+        this.books = initialData;
+        this.saveToLocalStorage(this.books);
+      }
+    }
+  
+    all() {
+      return this.books;
+    }
+  
+    add(newData) {
+      if (!newData || !newData.id) {
+        return false;
+      }
+  
+      const newBook = new Book(newData);
+      this.books.push(newBook);
+      return this.saveToLocalStorage(this.books);
+    }
+  
+    remove(id) {
+      this.books = this.books.filter((book) => book.id !== id);
+      return this.saveToLocalStorage(this.books);
+    }
+  }
